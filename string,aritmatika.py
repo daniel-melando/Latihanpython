fc8f6b7f6733b6d06228585e548583e4a49f7409
daniel-melando <dunniel21@gmail.com>
2025-09-04 23:11:00 +0700

    ubah value

---
+++ string,aritmatika.py
@@ -0,0 +1,17 @@
+# String
+nama = 'Iqbal'
+# Int
+umur = 20
+# Float
+tinggi = 168.7
+# Boolean
+statusMenikah = True
+# Aritmatika
+a = 1
+b = 2.5
+c = a + b
+print(f"Nama: {nama}")
+print(umur)
+print(tinggi)
+print(f"Status: {'Sudah Kawin' if statusMenikah else 'Belum Kawin'}")
+print(c)
